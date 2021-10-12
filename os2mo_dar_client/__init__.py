41e6197574719115e9e65ea91b5d0a11cb041df8
Emil Madsen <sovende@gmail.com>
2021-10-12 14:17:10 +0200

    Bump version to 0.2.0

--- os2mo_dar_client/__init__.py
+++ os2mo_dar_client/__init__.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from os2mo_dar_client.dar_client import AsyncDARClient
 from os2mo_dar_client.dar_client import DARClient
--- tests/test_os2mo_dar_client.py
+++ tests/test_os2mo_dar_client.py
@@ -5,4 +5,4 @@ from os2mo_dar_client import __version__
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.2.0"
